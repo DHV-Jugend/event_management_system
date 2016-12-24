f7fb526bd65d01aa94e950e5500eaf0a9424e6e1
Christoph Bessei <chris@bessei-it.eu>
2016-12-24 09:33:03 +0100

    Clean up, add autoload TODO

--- src/event-management-system.php
+++ src/event-management-system.php
@@ -1,71 +1,68 @@
 <?php
 
-class Event_Management_System {
+class Event_Management_System
+{
 
-	private static $plugin_path = null;
-	private static $plugin_url = null;
-	private static $src_directories = array(
-		'controller',
-		'../lib',
-		'model',
-		'view',
-		'utility',
-		'plugin_management',
-		'../../../../wp-includes',
-		'abstract',
-		'interface',
-	);
+    private static $plugin_path = null;
+    private static $plugin_url = null;
+    private static $src_directories = array(
+        'controller',
+        '../lib',
+        'model',
+        'view',
+        'utility',
+        'plugin_management',
+        '../../../../wp-includes',
+        'abstract',
+        'interface',
+    );
 
-	public function __construct( $plugin_path = null, $plugin_url = null ) {
-
-		//add_filter( 'fum_option_page_entries', array( $this, 'remove_fum_option_page' ) );
-
-		spl_autoload_register( array( $this, 'autoload' ) );
-
-
-		Event_Management_System::$plugin_path = plugin_dir_path( __FILE__ );
-		Event_Management_System::$plugin_url  = plugin_dir_url( __FILE__ );
-
-		Ems_Initialisation::initiate_plugin();
-	}
-
-//	public function remove_fum_option_page( $options ) {
-//		return array();
-//	}
+    public function __construct($plugin_path = null, $plugin_url = null)
+    {
+        //TODO Use composer autoload
+        spl_autoload_register(array($this, 'autoload'));
 
 
-	public function autoload( $class_name ) {
+        Event_Management_System::$plugin_path = plugin_dir_path(__FILE__);
+        Event_Management_System::$plugin_url = plugin_dir_url(__FILE__);
 
-		//Because of sucking wordpress name conventions class name != file name, convert it manually
-		$class_name = 'class-' . strtolower( str_replace( '_', '-', $class_name ) . '.php' );
-		if ( file_exists( Event_Management_System::$plugin_path . $class_name ) ) {
-			require_once( Event_Management_System::$plugin_path . $class_name );
+        Ems_Initialisation::initiate_plugin();
+    }
 
-			return;
-		}
+    public function autoload($class_name)
+    {
 
-		foreach ( self::$src_directories as $dir ) {
-			$dir  = trailingslashit( $dir );
-			$path = Event_Management_System::$plugin_path . $dir . $class_name;
-			if ( file_exists( $path ) ) {
-				require_once( $path );
+        //Because of sucking wordpress name conventions class name != file name, convert it manually
+        $class_name = 'class-' . strtolower(str_replace('_', '-', $class_name) . '.php');
+        if (file_exists(Event_Management_System::$plugin_path . $class_name)) {
+            require_once(Event_Management_System::$plugin_path . $class_name);
 
-				return;
-			}
-		}
-	}
+            return;
+        }
 
-	public
-	static function get_plugin_path() {
-		return self::$plugin_path;
-	}
+        foreach (self::$src_directories as $dir) {
+            $dir = trailingslashit($dir);
+            $path = Event_Management_System::$plugin_path . $dir . $class_name;
+            if (file_exists($path)) {
+                require_once($path);
 
-	/**
-	 * @return null
-	 */
-	public static function get_plugin_url() {
-		return self::$plugin_url;
-	}
+                return;
+            }
+        }
+    }
+
+    public static function get_plugin_path()
+    {
+        return self::$plugin_path;
+    }
+
+    /**
+     * @return null
+     */
+    public static function get_plugin_url()
+    {
+        return self::$plugin_url;
+    }
 
 }
 
