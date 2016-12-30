a9b6c721cc1a465d5a63069a713dfa519dc51f1f
Christoph Bessei <chris@bessei-it.eu>
2016-12-30 21:04:40 +0100

    Remove Fum_Uninstallation::uninstall_plugin(); since FUM doesn't require EMS

--- src/plugin_management/class-ems-uninstallation.php
+++ src/plugin_management/class-ems-uninstallation.php
@@ -4,17 +4,17 @@
  * @author Christoph Bessei
  * @version
  */
-class Ems_Uninstallation {
-	public static function uninstall_plugin() {
-		Fum_Uninstallation::uninstall_plugin();
-		error_log( 'ems_uninstall_plugin' );
-		$role = get_role( 'administrator' );
-		//TODO Because performance is not important here, maybe it would be nice if we just include all classes from the autoloader and the call get_admin_capabilities on each child of Ems_Post
-		//This avoids 'dead' capabilities
-		$caps = array_merge( Ems_Event::get_admin_capabilities(), Ems_Event_Daily_News::get_admin_capabilities(), Ems_Event_Daily_news::get_admin_capabilities() );
-		foreach ( $caps as $key => $value ) {
-			$role->remove_cap( $key );
-		}
-		remove_role( 'eventleiter' );
-	}
+class Ems_Uninstallation
+{
+    public static function uninstall_plugin()
+    {
+        $role = get_role('administrator');
+        //TODO Because performance is not important here, maybe it would be nice if we just include all classes from the autoloader and the call get_admin_capabilities on each child of Ems_Post
+        //This avoids 'dead' capabilities
+        $caps = array_merge(Ems_Event::get_admin_capabilities(), Ems_Event_Daily_News::get_admin_capabilities());
+        foreach ($caps as $key => $value) {
+            $role->remove_cap($key);
+        }
+        remove_role('eventleiter');
+    }
 } 
