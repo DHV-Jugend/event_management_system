c8dbf861b6bbe8c553b3f23081a8f84908f2fc26
Christoph Bessei <chris@bessei-it.eu>
2017-01-04 13:51:01 +0100

    Remove sausage/sauce from lib

--- lib/sauce/vendor/brianium/paratest/test/fixtures/small-tests/FastUnitTestTemplate.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/BaseUnitTestCase.php';
-
-class FastUnitTestTemplate extends BaseUnitTestCase {
-}
--- lib/sauce/vendor/phpunit/phpunit/Tests/_files/ThrowExceptionTestCase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ThrowExceptionTestCase extends PHPUnit_Framework_TestCase {
-	public function test() {
-		throw new RuntimeException;
-	}
-}
