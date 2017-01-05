06cc2c0fc8e51aaf26f4b44161b3a3159924ace8
Christoph Bessei <chris@bessei-it.eu>
2017-01-05 22:58:54 +0100

    Install wp-tests with composer scripts instead of travis.yml

---
+++ src/Classes/Composer/InstallTests.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Christoph Bessei
+ * @version
+ */
+
+namespace BIT\EMS\Composer;
+
+
+class InstallTests
+{
+    public static function run()
+    {
+        $pluginDir = realpath(__DIR__ . "/../../../..");
+        // Install wp tests
+        passthru('bash ' . $pluginDir . '/event-management-system/tests/bin/install-wp-tests.sh wordpress_test root "" localhost $WP_VERSION');
+        
+        $frontendUserManagementDir = $pluginDir . '/frontend-user-management';
+        if (!file_exists($frontendUserManagementDir)) {
+            // Install frontend-user-management
+            passthru('git clone https://github.com/SchwarzwaldFalke/frontend-user-management ' . $frontendUserManagementDir);
+        }
+    }
+}
