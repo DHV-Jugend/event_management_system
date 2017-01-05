ade8049ecb570c9eb74574704fb6d91345faad04
Christoph Bessei <chris@bessei-it.eu>
2017-01-05 22:20:45 +0100

    Remove unused tests, remove examples, load frontend_user_management (since it's needed for tests), add events before tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,8 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/event-management-system.php';
+    require dirname( dirname( __FILE__ ) ) . '/../frontend-user-management/frontend-user-management.php';
+    require dirname( dirname( __FILE__ ) ) . '/event-management-system.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
--- tests/phpunit/class-ems-event-test.php
+++ tests/phpunit/class-ems-event-test.php
@@ -4,74 +4,99 @@
  * @author Christoph Bessei
  * @version
  */
-class Ems_Event_Test extends WP_UnitTestCase {
+class Ems_Event_Test extends WP_UnitTestCase
+{
 
-	public function setUp() {
-		parent::setUp();
-	}
+    public function setUp()
+    {
+        parent::setUp();
 
-	public function test_get_events_limit_with_sort() {
-		$events = Ems_Event::get_events( 5 );
-		$this->assertEquals( 5, count( $events ) );
-	}
+        // Add events
+        for ($i = 0; $i < 10; $i++) {
+            $post = array(
+                'comment_status' => 'closed',
+                'ping_status' => 'closed',
+                'post_author' => get_current_user_id(),
+                'post_status' => 'publish',
+                'post_type' => Ems_Event::get_post_type(),
+                'post_content' => "TestContent" . $i,
+                'post_name' => "RandomEvent" . $i,
+                'post_title' => "RandomEventTitle" . $i
+            );
 
-	public function test_get_events_limit_without_sort() {
-		$events = Ems_Event::get_events( 5, false );
-		$this->assertEquals( 5, count( $events ) );
-	}
+            wp_insert_post($post);
+        }
+    }
 
-	public function test_get_events_limit_zero_with_sort() {
-		$events = Ems_Event::get_events( 0, true );
-		$this->assertEquals( 0, count( $events ) );
-	}
+    public function test_get_events_limit_with_sort()
+    {
+        $events = Ems_Event::get_events(5);
+        $this->assertEquals(5, count($events));
+    }
 
-	public function test_get_events_limit_zero_without_sort() {
-		$events = Ems_Event::get_events( 0, false );
-		$this->assertEquals( 0, count( $events ) );
-	}
+    public function test_get_events_limit_without_sort()
+    {
+        $events = Ems_Event::get_events(5, false);
+        $this->assertEquals(5, count($events));
+    }
 
-	/**
-	 * Tests if the returned event array is really sorted (used compare fuction is Ems_Event->compare)
-	 */
-	public function test_get_events_sorted() {
-		$events = Ems_Event::get_events();
+    public function test_get_events_limit_zero_with_sort()
+    {
+        $events = Ems_Event::get_events(0, true);
+        $this->assertEquals(0, count($events));
+    }
 
-		$this->assertGreaterThan( 1, count( $events ), 'At least two elements are needed to check order' );
+    public function test_get_events_limit_zero_without_sort()
+    {
+        $events = Ems_Event::get_events(0, false);
+        $this->assertEquals(0, count($events));
+    }
 
-		for ( $i = 1; $i < count( $events ); $i ++ ) {
-			$first_start_date  = $events[$i - 1]->get_start_date_time();
-			$second_start_date = $events[$i]->get_start_date_time();
+    /**
+     * Tests if the returned event array is really sorted (used compare fuction is Ems_Event->compare)
+     */
+    public function test_get_events_sorted()
+    {
+        $events = Ems_Event::get_events();
 
-			$this->assertFalse( $first_start_date > $second_start_date, "start date of first event shouldn't be greater then start date of second event" );
+        $this->assertGreaterThan(1, count($events), 'At least two elements are needed to check order');
 
-			$first_end_date  = $events[$i - 1]->get_end_date_time();
-			$second_end_date = $events[$i]->get_end_date_time();
+        for ($i = 1; $i < count($events); $i++) {
+            $first_start_date = $events[$i - 1]->get_start_date_time();
+            $second_start_date = $events[$i]->get_start_date_time();
 
-			//if start date is equal and end date differs, check if the elements are ordered by end date
-			if ( $first_start_date == $second_start_date && $first_end_date != $second_end_date ) {
-				$this->assertTrue( $first_end_date < $second_end_date, 'start dates are equal and end dates differ but end date of first element is great then end date of second element. first_end_date: ' . $first_end_date->getTimestamp() . ' seocond_end_date: ' . $second_end_date->getTimestamp() );
-			}
-		}
-	}
+            $this->assertFalse($first_start_date > $second_start_date, "start date of first event shouldn't be greater then start date of second event");
 
-	public function test_get_events_limit() {
-		$this->assertEquals( 5, count( Ems_Event::get_events( 5 ) ) );
-		$this->assertEquals( 1, count( Ems_Event::get_events( 1 ) ) );
+            $first_end_date = $events[$i - 1]->get_end_date_time();
+            $second_end_date = $events[$i]->get_end_date_time();
 
-		$this->assertEquals( 5, count( Ems_Event::get_events( 5, false ) ) );
-		$this->assertEquals( 1, count( Ems_Event::get_events( 1, false ) ) );
+            //if start date is equal and end date differs, check if the elements are ordered by end date
+            if ($first_start_date == $second_start_date && $first_end_date != $second_end_date) {
+                $this->assertTrue($first_end_date < $second_end_date, 'start dates are equal and end dates differ but end date of first element is great then end date of second element. first_end_date: ' . $first_end_date->getTimestamp() . ' seocond_end_date: ' . $second_end_date->getTimestamp());
+            }
+        }
+    }
 
-	}
+    public function test_get_events_limit()
+    {
+        $this->assertEquals(5, count(Ems_Event::get_events(5)));
+        $this->assertEquals(1, count(Ems_Event::get_events(1)));
 
-	public function test_get_events_reverse() {
-		$events         = Ems_Event::get_events();
-		$reverse_events = Ems_Event::get_events( - 1, true, true );
+        $this->assertEquals(5, count(Ems_Event::get_events(5, false)));
+        $this->assertEquals(1, count(Ems_Event::get_events(1, false)));
 
-		$reverse_events2 = array_reverse( $events );
+    }
 
-		foreach ( $reverse_events as $key => $event ) {
-			$this->assertEquals( $event->get_post()->post_title, $reverse_events2[$key]->get_post()->post_title );
-		}
-	}
+    public function test_get_events_reverse()
+    {
+        $events = Ems_Event::get_events();
+        $reverse_events = Ems_Event::get_events(-1, true, true);
+
+        $reverse_events2 = array_reverse($events);
+
+        foreach ($reverse_events as $key => $event) {
+            $this->assertEquals($event->get_post()->post_title, $reverse_events2[$key]->get_post()->post_title);
+        }
+    }
 }
  
