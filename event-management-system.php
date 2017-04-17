089dbbd5878483507a917ab7faeac802c798213d
Christoph Bessei <chris@bessei-it.eu>
2017-04-17 22:46:50 +0200

    Bump version to 0.1.0, start using semantic versioning

--- event-management-system.php
+++ event-management-system.php
@@ -3,7 +3,7 @@
  * Plugin Name: Event Management System
  * Plugin URI: https://github.com/SchwarzwaldFalke/event_management_system
  * Description: Manage your events with WordPress (create, edit, delete, get list of participants, show some event statistics,...)
- * Version: 0.09
+ * Version: 0.1.0
  * Text Domain: event-management-system
  * Domain Path: /languages
  * Author: Christoph Bessei
@@ -11,12 +11,11 @@
  * License: GPLv2
  */
 
-
 //Load plugin
-require_once("src/event-management-system.php");
+require_once(__DIR__ . "src/event-management-system.php");
 new Event_Management_System(plugin_dir_path(__FILE__), plugin_dir_url(__FILE__));
 
 //Have to be called from main plugin file? Couldn't get it working in other places
-register_activation_hook(__FILE__, array('Ems_Activation', 'activate_plugin'));
-register_deactivation_hook(__FILE__, array('Ems_Deactivation', 'deactivate_plugin'));
-register_uninstall_hook(__FILE__, array('Ems_Uninstallation', 'uninstall_plugin'));
+register_activation_hook(__FILE__, ['Ems_Activation', 'activate_plugin']);
+register_deactivation_hook(__FILE__, ['Ems_Deactivation', 'deactivate_plugin']);
+register_uninstall_hook(__FILE__, ['Ems_Uninstallation', 'uninstall_plugin']);
