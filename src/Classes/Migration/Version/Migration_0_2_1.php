999f8f16cd7428849b88d50f6647eaf8cdd9d1d3
Christoph Bessei <chris@bessei-it.eu>
2017-12-23 11:15:34 +0100

    * Fix ems_event start-/end-date migration path

--- src/Classes/Migration/Version/Migration_0_2_1.php
+++ src/Classes/Migration/Version/Migration_0_2_1.php
@@ -29,24 +29,34 @@ class Migration_0_2_1 implements MigrationInterface
     {
         \Ems_Event::get_post_type();
         $updatedEntries = 0;
-        $events = get_posts(['post_type' => Event::get_post_type(), 'numberposts' => -1]);
+        $events = get_posts(['post_type' => Event::get_post_type(), 'post_status' => 'any', 'numberposts' => -1]);
+        $checkedEvents = [];
         if (is_array($events)) {
             /** @var \WP_Post $event */
             foreach ($events as $event) {
+
+                $updated = false;
                 $startDate = get_post_meta($event->ID, EventMetaBox::EVENT_START_DATE, true);
                 if ($startDate instanceof \DateTime) {
                     update_post_meta($event->ID, EventMetaBox::EVENT_START_DATE, $startDate->getTimestamp());
+                    $updated = true;
                 }
 
                 $endDate = get_post_meta($event->ID, EventMetaBox::EVENT_END_DATE, true);
                 if ($endDate instanceof \DateTime) {
                     update_post_meta($event->ID, EventMetaBox::EVENT_END_DATE, $endDate->getTimestamp());
+                    $updated = true;
                 }
 
-                $updatedEntries++;
+                if ($updated) {
+                    $updatedEntries++;
+                }
+
+                $checkedEvents[] = $event->post_title . ' (' . $event->ID . ')';
             }
         }
 
-        $this->resultMessage = 'Migrated ' . $updatedEntries . ' events to new date format.';
+        $this->resultMessage = 'Migrated ' . $updatedEntries . ' events to new date format.<br>';
+        $this->resultMessage .= 'Checked events:<br>' . implode('<br>', $checkedEvents);
     }
 }
